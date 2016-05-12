7966ee4d32109d32914f0501fafd1f7bb3a4918b
KIM Taegyoon <steloflute@gmail.com>
2016-05-12 19:37:42 +0900

    1

---
+++ readme.txt
@@ -0,0 +1,18 @@
+# OracleRun
+
+Read Oracle SQL file and execute line by line.
+
+Generate output file as TSV (tab-separated value) file.
+
+## Requirement
+Oracle JDBC driver (Put ojdbc*.jar to bin/.)
+
+## Compile
+```
+javac src/Main.java
+```
+
+## Run
+```
+java -cp bin;bin/ojdbc*.jar Main
+```
---
+++ src/Main.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		String db_ip, db_port, db_sid; 
+		String db_user;
+		String db_password;
+		final String inputFile;
+		
+		System.out.print("DB IP: ");
+		db_ip = in.readLine();
+		
+		System.out.print("DB port(e.g. 1521): ");
+		db_port = in.readLine();
+		
+		System.out.print("DB SID: ");
+		db_sid = in.readLine();
+		
+		System.out.print("DB User: ");
+		db_user = in.readLine();
+		
+		System.out.print("DB Password: ");
+		db_password = in.readLine();
+
+		System.out.print("Input SQL file: ");
+		inputFile = in.readLine();
+		
+		// "jdbc:oracle:thin:@IP:PORT:SID";
+		String db_url = String.format("jdbc:oracle:thin:@%s:%s:%s", db_ip, db_port, db_sid);
+		System.err.println("DB URL: " + db_url);
+
+		try {
+			Connection conn;
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			conn = DriverManager.getConnection(db_url, db_user, db_password);
+			
+			BufferedReader br = new BufferedReader(new FileReader(inputFile));
+			int count = 0;
+			while (true) {
+				count++;
+				Statement stmt;
+				ResultSet rs;
+				
+				// String query = "select * from foo";
+				String query = br.readLine();
+				if (query == null) break;
+				
+				System.err.println(count + " " + query);
+				
+				stmt = conn.createStatement();
+
+				rs = stmt.executeQuery(query);
+
+				while (rs.next()) {
+					int numCol = rs.getMetaData().getColumnCount();
+					for (int i = 1; i <= numCol; i++) {
+						if (i > 1)
+							System.out.print("\t");
+						System.out.print(rs.getString(i));
+					}
+					System.out.println();
+				}
+				stmt.close();
+				rs.close();				
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+	}
+}
